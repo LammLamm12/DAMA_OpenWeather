f71b20d08a169966cdce048fbe17dc1322cbb02d
Luís Mendes <jluis.lam1@gmail.com>
2023-07-02 20:26:00 +0100

    Fixed translation and standardized looks

--- app/src/main/java/pt/ipt/dama/dama_project2/Translation.kt
+++ app/src/main/java/pt/ipt/dama/dama_project2/Translation.kt
@@ -1,7 +1,9 @@
 package pt.ipt.dama.dama_project2
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.RadioGroup
 import androidx.appcompat.app.AppCompatActivity
 import java.util.*
@@ -15,23 +17,32 @@ class Translation : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.transfrag)
 
-        setSupportActionBar(findViewById(R.id.toolbar))
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         radioGroup = findViewById(R.id.language_radio_group)
         saveButton = findViewById(R.id.button_save)
 
         saveButton.setOnClickListener {
-            val selectedRadioButtonId = radioGroup.checkedRadioButtonId
-            val selectedLanguage = when (selectedRadioButtonId) {
+            val selectedLanguage = when (radioGroup.checkedRadioButtonId) {
                 R.id.radio_english -> "en"
                 R.id.radio_portuguese -> "pt"
                 else -> "en"
             }
 
             setAppLanguage(selectedLanguage)
+            // necessary to change language in main_activity
+            val intent = Intent(this, MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            startActivity(intent)
             recreate()
         }
+
+        // back button
+
+        val backButton = findViewById<ImageView>(R.id.backButton)
+
+        backButton.setOnClickListener { finish() }
+
+
     }
 
     private fun setAppLanguage(languageCode: String) {
